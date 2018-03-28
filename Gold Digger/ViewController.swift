77f2ad51f600c4a58e71f1ec515a8091e680d005
Dean Lindsey <dlindsey@dml-macbook-pro>
2018-03-28 17:05:58 +0100

    Finished Test App

--- Gold Digger/ViewController.swift
+++ Gold Digger/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var activeProduct : SKProduct?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SKPaymentQueue.default().add(self)
+        
+        let productIds : Set<String> = ["uk.org.coolamber.GoldDigger.Gold"]
+        let prodRequest = SKProductsRequest(productIdentifiers: productIds)
+        prodRequest.delegate = self
+        prodRequest.start()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buyTapped(_ sender: Any) {
+        
+        if let activeProduct = activeProduct {
+            print("Buying....")
+            let payment = SKPayment(product: activeProduct)
+            SKPaymentQueue.default().add(payment)
+        }
+        
+        
     }
-
-
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        
+        for transaction in transactions {
+            switch (transaction.transactionState) {
+                
+            case .purchased :
+                SKPaymentQueue.default().finishTransaction(transaction)
+                resultLabel.text = "YOU GOT SOME GOLD!"
+            case .failed :
+                SKPaymentQueue.default().finishTransaction(transaction)
+                resultLabel.text = "Something went wrong :-("
+            default :
+                break
+            }
+        }
+    }
+    
+    func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+        
+        print("Loaded Products")
+        
+        for product in response.products {
+            
+            print("Product: \(product.productIdentifier) \(product.localizedTitle) \(product.price.floatValue)")
+            
+            nameLabel.text = product.localizedTitle
+            activeProduct = product
+        }
+    }
+    
+    
+    
 }
 
